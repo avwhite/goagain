eaf6302c4648056bab6b35308d48c96517e2e2b3
Andreas Vinter-Hviid <andreas.vinterhviid@gmail.com>
2016-05-31 20:15:02 +0200

    Draw lines and hoshi on board

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,55 @@ use gtk::prelude::*;
 use goagain::goban;
 
 fn draw_board(w : &gtk::DrawingArea, ctx : &cairo::Context) -> Inhibit {
+    let lines = 19; //should be parameterzied.
+
     let height = w.get_allocated_height() as f64;
     let width = w.get_allocated_width() as f64;
     let s = std::cmp::min(
         w.get_allocated_height(),
         w.get_allocated_width()
     ) as f64;
+    let x = (width - s)/2.0;
+    let y = (height - s)/2.0;
+
+    //Draw board
     ctx.set_source_rgb(0.78, 0.86, 0.52);
-    ctx.rectangle((width - s)/2.0, (height-s)/2.0, s, s);
+    ctx.rectangle(x, y, s, s);
     ctx.fill();
+
+    //Draw lines
+    let line_diff = s/(lines as f64); 
+    let inner_x = x + line_diff/2.0;
+    let inner_y = y + line_diff/2.0;
+    let inner_s = s - line_diff;
+
+    ctx.set_source_rgb(0.0, 0.0, 0.0);
+    ctx.set_line_width(1.0);
+    for i in 0..lines {
+        ctx.move_to(inner_x, inner_y + (i as f64)*line_diff);
+        ctx.line_to(inner_x + inner_s, inner_y + (i as f64)*line_diff);
+
+        ctx.move_to(inner_x + (i as f64)*line_diff, inner_y);
+        ctx.line_to(inner_x + (i as f64)*line_diff, inner_y + inner_s);
+    }
+    ctx.stroke();
+
+    //Draw hoshi for known board sizes
+    if lines == 19 {
+        let radius = line_diff/12.0;
+        let coords = vec![3, 9, 15];
+        for i in &coords {
+            for j in &coords {
+                ctx.arc(
+                    inner_x + (*i as f64)*line_diff,
+                    inner_y + (*j as f64)*line_diff,
+                    radius, radius, 4.0 * 3.14
+                );
+                ctx.fill();
+            }
+        }
+    }
+
     Inhibit(false)
 }
 
